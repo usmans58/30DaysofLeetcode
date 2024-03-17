6671215d934f4ef36ac797d84f635226f4a6d5b7
Usman Siddiqui <90143991+usmans58@users.noreply.github.com>
2024-03-17 15:30:49 +0500

    Time: 2 ms (65.92%), Space: 5.6 MB (27.37%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -0,0 +1,17 @@
+bool isPerfectSquare(int num) {
+   
+   int low=1,high=num;
+    long long mid;
+    while(low<=high){
+        mid=low+(high-low)/2;
+        
+        if(mid*mid==num)
+            return true;
+        else if(mid*mid<num)
+            low=mid+1;
+        else
+            high=mid-1;
+        
+    }
+    return false;
+}
